dc895af36740945bb971a23efc6eb65899314ec2
Jaime Valdemoros <JaimeValdemoros@users.noreply.github.com>
2018-05-25 22:08:32 +0100

    Make reads and writes linearisable and update reference

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,8 @@ use std::ops::{Add, AddAssign};
 use std::sync::Arc;
 use std::sync::atomic::{AtomicIsize, AtomicUsize, Ordering};
 
+pub mod reference;
+
 pub struct RingBufferReader<T> {
     inner: Arc<RingBuffer<T>>,
 }
@@ -107,12 +109,16 @@ impl<T: Send> RingBuffer<T> {
         }
     }
 
+    // invariant: self.head % self.size is empty
     pub fn write(&self, x: T) {
         let head = self.head.load(Ordering::SeqCst);
         // Get the pointer to the next slot, which is guaranteed to be empty.
         let head_ptr: *mut Option<T> = self.data[head].get();
         let head_ptr: &mut Option<T> = unsafe { &mut *head_ptr };
         *head_ptr = Some(x);
+
+        // linearisation point if we exit because head - tail.value < self.size:
+        self.head.store(head + 1, Ordering::SeqCst);
 
         loop {
             let tail: Tail = self.tail.load(Ordering::SeqCst).into();
@@ -125,22 +131,18 @@ impl<T: Send> RingBuffer<T> {
                 continue;
             }
 
+            // linearisation point if we exit because we pushed the tail pointer:
             if self.tail
                 .compare_and_swap(tail.into(), (tail + 1).into(), Ordering::SeqCst)
                 == tail.into()
             {
-                let tail_ptr: *mut Option<T> = self.data[tail.value].get();
-                let tail_ptr: &mut Option<T> = unsafe { &mut *tail_ptr };
-                *tail_ptr = None;
                 break;
             }
         }
-
-        self.head.store(head + 1, Ordering::SeqCst);
     }
 
     pub fn read(&self) -> RingIter<T> {
-        let tail = loop {
+        let mut tail = loop {
             let tail: Tail = self.tail.load(Ordering::SeqCst).into();
             assert!(!tail.locked);
             let mut new_tail = tail;
@@ -152,7 +154,29 @@ impl<T: Send> RingBuffer<T> {
                 break new_tail;
             }
         };
+
+        // The head may have been pushed some number of times since we successfully read and locked
+        // the tail, but the tail has not been moved (as the reading thread does not try to push
+        // the tail if it is locked).
+        // If the head is no more than 'size' away from the tail, then we can take the linearisation
+        // point as here. The linearisation points of the other thread will be at the moments it
+        // updated the head, as it has not hit the case where it had to push the tail as well.
+        //
+        // If the head is `size+1` away from the tail, then the other thread has push until it hit
+        // the tail, then gotten stuck as the tail was locked. In this case we can help it out by
+        // pushing the tail ourselves. We do not try to pull out the data ourselves as the other
+        // thread might overwrite it in the meantime via the head pointer. Rather, the writing
+        // thread will implicitly drop the contents of this slot when it writes to that slot.
+        //
+        // In the former case, we set this to be the linearisation point. In the latter, we set the
+        // linearisation point to the moment we pushed the tail.
         let head = self.head.load(Ordering::SeqCst);
+
+        if head - tail.value == self.size + 1 {
+            tail += 1;
+            self.tail.store(tail.into(), Ordering::SeqCst);
+        }
+
         RingIter {
             fixed_head: head,
             current_tail: tail,
@@ -183,6 +207,7 @@ impl<'a, T> Iterator for RingIter<'a, T> {
             if self.current_tail.value == self.fixed_head {
                 self.current_tail.locked = false;
             }
+
             self.inner
                 .tail
                 .store(self.current_tail.into(), Ordering::SeqCst);
--- src/reference.rs
+++ src/reference.rs
@@ -1,55 +1,18 @@
-use std::iter::once;
-use rand::{Rand, Rng};
-
 pub struct BoundedBuffer<T> {
     size: usize,
     data: Vec<T>,
 }
 
-pub trait Apply {
-    type Op: Rand;
-    type OpRes;
-    fn apply(&mut self, op: Self::Op) -> Self::OpRess;
-}
-
-pub enum Ops<T> {
-    Push(T),
-    Pop,
-}
-
-impl<T: Rand> Rand for Ops<T> {
-    fn rand<R: Rng>(rng: &mut R) -> Self {
-        <Option<T> as Rand>::rand(rng).map_or(Ops::Pop, Ops::Push)
-    }
-}
-
-pub enum OpRes<T> {
-    Pushed,
-    PoppedEmpty,
-    PoppedGot(T),
-}
-
-impl<T> Apply for RingBuffer<T> {
-    type Op = Ops<T>;
-    type OpRes = OpRes<T>;
-    fn apply(&mut self, op: Self::Op) -> Self::OpRes {
-        match op {
-            Ops::Push(x) => { self.push(x); OpRes::Pushed }
-            Ops::Pop => self.read().map_or(OpRes::PoppedEmpty, OpRes::PoppedGot),
-        }
-    }
-}
-
-impl<T> RingBuffer<T> {
+impl<T> BoundedBuffer<T> {
     pub fn new(size: usize) -> Self {
         assert!(size > 0);
-        RingBuffer {
+        BoundedBuffer {
             size,
-            data: Vec::with_capacity(size),
+            data: vec![],
         }
     }
 
-    pub fn write(&mut self, x: T) -> Self {
+    pub fn write(&mut self, x: T) {
         if self.data.len() < self.size {
             self.data.push(x)
         } else {
@@ -58,12 +21,7 @@ impl<T> RingBuffer<T> {
         }
     }
 
-    pub fn read(&mut self) -> Option<T> {
-        if self.data.is_empty() {
-            None
-        } else {
-            self.data.rotate_left(1);
-            self.data.pop()
-        }
+    pub fn read(&mut self) -> Vec<T> {
+        ::std::mem::replace(&mut self.data, vec![])
     }
 }
